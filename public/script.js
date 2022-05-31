866984c535c11136534f84fbb4d45e5449dbc45e
gemmau <100390799+gemmau@users.noreply.github.com>
2022-05-31 14:14:20 +1000

    commit pre_bootstrap

--- public/script.js
+++ public/script.js
@@ -1,66 +1,125 @@
-// Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
-const button = document.querySelector("#taskform > button"); // Complex CSS query
-const tasklist = document.getElementById("tasklist");
-const taskInput = document.getElementById("taskInput");
+const todos = document.querySelectorAll(".todo");
+const all_status = document.querySelectorAll(".status");
+let draggableTodo = null;
 
-// Event listener for Button click
-// This could also be form.addEventListener("submit", function() {...} )
-button.addEventListener("click", function(event) {
-  event.preventDefault(); // Not as necessary for button, but needed for form submit
+todos.forEach((todo) => {
+  todo.addEventListener("dragstart", dragStart);
+  todo.addEventListener("dragend", dragEnd);
+});
 
-  let task = form.elements.task.value; // could be swapped out for line below
-  //let task = taskInput.value;
-
-  let date = (new Date()).toLocaleDateString('en-US') //Convert to short date format
-
-  // Call the addTask() function using
-  addTask(task, date, "26/03/2021", "Low", ["1", "30"], false);
-
-  // Log out the newly populated taskList everytime the button has been pressed
-  console.log(taskList);
-})
-
-// Create an empty array to store our tasks
-var taskList = [];
-
-function addTask(taskDescription, createdDate, dueDate, priorityRating, estimatedTime, completionStatus) {
-  let task = {
-    taskDescription,
-    createdDate,
-    dueDate,
-    priorityRating,
-    estimatedTime,
-    completionStatus
-  };
-
-  // Add the task to our array of tasks
-  taskList.push(task);
-
-  // Separate the DOM manipulation from the object creation logic
-  renderTask(task);
+function dragStart() {
+  draggableTodo = this;
+  setTimeout(() => {
+    this.style.display = "none";
+  }, 0);
+  console.log("dragStart");
 }
 
-
-// Function to display the item on the page
-function renderTask(task) {
-  let item = document.createElement("li");
-  item.innerHTML = "<p>" + task.taskDescription + "</p>";
-
-  tasklist.appendChild(item);
-
-  // Setup delete button DOM elements
-  let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete");
-  delButton.appendChild(delButtonText);
-  item.appendChild(delButton); // Adds a delete button to every task
-
-  // Listen for when the 
-  delButton.addEventListener("click", function(event){
-    item.remove(); // Remove the task item from the page when button clicked
-    // Because we used 'let' to define the item, this will always delete the right element
-  })
-  
-  // Clear the value of the input once the task has been added to the page
-  form.reset();
+function dragEnd() {
+  draggableTodo = null;
+  setTimeout(() => {
+    this.style.display = "block";
+  }, 0);
+  console.log("dragEnd");
 }
+
+all_status.forEach((status) => {
+  status.addEventListener("dragover", dragOver);
+  status.addEventListener("dragenter", dragEnter);
+  status.addEventListener("dragleave", dragLeave);
+  status.addEventListener("drop", dragDrop);
+});
+
+function dragOver(e) {
+  e.preventDefault();
+  //   console.log("dragOver");
+}
+
+function dragEnter() {
+  this.style.border = "1px dashed #ccc";
+  console.log("dragEnter");
+}
+
+function dragLeave() {
+  this.style.border = "none";
+  console.log("dragLeave");
+}
+
+function dragDrop() {
+  this.style.border = "none";
+  this.appendChild(draggableTodo);
+  console.log("dropped");
+}
+
+/* modal */
+const btns = document.querySelectorAll("[data-target-modal]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.targetModal).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
+
+/* create todo  */
+const todo_submit = document.getElementById("todo_submit");
+
+todo_submit.addEventListener("click", createTodo);
+
+function createTodo() {
+  const todo_div = document.createElement("div");
+  const input_val = document.getElementById("todo_input").value;
+  const txt = document.createTextNode(input_val);
+
+  todo_div.appendChild(txt);
+  todo_div.classList.add("todo");
+  todo_div.setAttribute("draggable", "true");
+
+  /* create span */
+  const span = document.createElement("span");
+  const span_txt = document.createTextNode("\u00D7");
+  span.classList.add("close");
+  span.appendChild(span_txt);
+
+  todo_div.appendChild(span);
+
+  no_status.appendChild(todo_div);
+
+  span.addEventListener("click", () => {
+    span.parentElement.style.display = "none";
+  });
+  //   console.log(todo_div);
+
+  todo_div.addEventListener("dragstart", dragStart);
+  todo_div.addEventListener("dragend", dragEnd);
+
+  document.getElementById("todo_input").value = "";
+  todo_form.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+const close_btns = document.querySelectorAll(".close");
+
+close_btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.style.display = "none";
+  });
+});
